3b83fd0b45d2bdbdeb268aeda19853de7af94b34
Gustavo <gustavohenriquealves418@gmail.com>
2024-05-23 21:50:03 -0300

    Configurando formulario Email

--- myproject/polls/forms.py
+++ myproject/polls/forms.py
@@ -1,4 +1,5 @@
 from django import forms 
+from django.core.mail import EmailMessage
 import re 
 
 class contatos(forms.Form):
@@ -7,4 +8,18 @@ class contatos(forms.Form):
     assunto = forms.CharField(label="Assunto",max_length=100)
     menssagem = forms.CharField(label="Menssagem",widget=forms.Textarea())
     def enviando_email(self):
-        pass  
+        nome = self.cleaned_data['Nome']
+        email = self.cleaned_data['email'] 
+        assunto = self.cleaned_data['assunto']
+        menssagem = self.cleaned_data['messagem'] 
+        corpo_email = f"Nome:{nome}\nMenssagem{menssagem}"
+        mail = EmailMessage(
+            subject=assunto,
+            para_email='gustavohenriquealves418@gmail.com',
+            to=[email,],
+            body=corpo_email,
+            headers={
+                'Replat-to':'gustavohenriquealves418@gmail.com'
+            }
+        )
+        mail.send()
--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages 
+from .forms import contatos 
 # Create your views here.
+
 def index(request):
     return render(request, 'polls/index.html')
 
 def contato(request):
-    return render(request, 'polls/contato.html')
-
+    if str(request.method) == 'POST':
+        form = contatos(request.POST)
+        if form.is_valid() :
+            form.send_email()
