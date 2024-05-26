7ef32b8d522b18f6a14e1d59379fe35c92f2d289
Gustavo <gustavohenriquealves418@gmail.com>
2024-05-26 18:30:12 -0300

    teste

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -117,11 +117,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR,"static")
-]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email 
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backend.console.EmailBackend"
+else:
+    EMAIL_BACKEND = "django.core.mail.backend.smtp.EmailBackend"
--- myproject/polls/tests.py
+++ myproject/polls/tests.py
@@ -1,10 +1,3 @@
 from django.test import TestCase
-import re 
-# Create your tests here.
-email = "aragorn122334gmail.com"
-padra_email = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
-resultado = re.search(padra_email,email)
-if resultado:
-    print("Email valido")
-else:
-    print("Email invalido")
+from django.core.mail import send_mail 
+
