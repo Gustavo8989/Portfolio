281fbfe508accbf9b699d69c9bab74086d5442d7
Gustavo <gustavohenriquealves418@gmail.com>
2024-05-26 20:13:22 -0300

    Email

--- myproject/polls/forms.py
+++ myproject/polls/forms.py
@@ -8,7 +8,6 @@ import qrcode
 class email(forms.Form):
     nome = forms.CharField(label="Nome",max_length=50)
     email = forms.EmailField(label="Email",max_length=100)
-    assunto = forms.CharField(label="Assunto",max_length=100)
     menssagem = forms.CharField(label="Menssagem",widget=forms.Textarea())
     def enviando_email(self):
         nome = self.cleaned_data['Nome']
